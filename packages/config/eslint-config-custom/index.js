15b3b326d10d11e0a01094fbb44e6470d10d1e20
Joni Vainio-Kaila <joni.vainio-kaila@vincit.fi>
2023-04-25 19:46:49 +0300

    Initial custom configuration of turborepo

---
+++ packages/config/eslint-config-custom/index.js
@@ -0,0 +1,29 @@
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    project: "./tsconfig.json",
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+    "@typescript-eslint/consistent-type-imports": 2,
+  },
+  ignorePatterns: [".eslintrc.cjs", "*.config.cjs", "*.config.mjs"],
+  reportUnusedDisableDirectives: true,
+};
